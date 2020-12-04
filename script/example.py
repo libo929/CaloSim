fd3503e5b32699320b2a70a89949a6cfb7ba8df4
Bo Li <libo0929@gmail.com>
2020-12-04 17:21:41 +0800

    updated example.py

--- script/example.py
+++ script/example.py
@@ -2,47 +2,54 @@
 
 import os
 import sys
+import math
+
 import SDHCALSim as sim
 
-import math
+particle = 'pi0'   # partile type 
+energy   = 40      # GeV
+#useUI    = False   # use User Interface
+useUI    = True # use User Interface
 
 
 if __name__ == '__main__' :
 
-        #os.environ["SIMEXE"] = '/path/to/SDHCALSim/bin/SDHCALSim'
-	os.environ["SIMEXE"] = '/Users/libo/workplace/ECALSim/bin/SDHCALSim'
+        exeDir = sys.path[0]
 
-	particle = 'pi0'
-	energy = 10
-
+        if useUI:
+            exeDir = os.path.join(exeDir, '../bin/SDHCALSimUI')
+        else:
+            exeDir = os.path.join(exeDir, '../bin/SDHCALSim')
+	os.environ["SIMEXE"] = exeDir
 
 	params = sim.Params()
 	params.physicsList = 'QGSP_BERT'
-	params.nEvent = 10
-	params.seed = 0
-	params.outputFileName = 'SDHCALSim'
+	params.nEvent = 1
+	params.seed = 1
+
+        if useUI:
+            params.outputFileName = 'SDHCALSimUI_' + particle + '_' + str(energy) + 'GeV'
+        else:
+            params.outputFileName = 'SDHCALSim_' + particle + '_' + str(energy) + 'GeV'
+
 	
 	params.oldConfig = False
 	params.killNeutrons = False
 
 
-	charged = sim.Particle()
-	charged.particleName = particle
-	charged.energy = energy
+	part = sim.Particle()
+	part.particleName = particle
+	part.energy = energy
 
 
-	charged.momentumOption = 'gaus'
-	#charged.sigmaMomentum = 0.15
-	charged.sigmaMomentum = 0.
+	part.momentumOption = 'gaus'
+	part.sigmaMomentum = 0.
 
-	charged.positionOption = 'gaus'
-	charged.positionZ = -1800
-	#charged.sigmaPos = 33
-	charged.sigmaPos = 0
+	part.positionOption = 'gaus'
+	part.positionZ = -1800           # mm
+	part.sigmaPos = 0
 
 
-	params.particleList.append(charged)
+	params.particleList.append(part)
 
 	sim.launch( params )
-
-
