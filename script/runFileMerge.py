5fa5ad5b3a651a4b810f3534eb9de4eb29b6e4ff
Bo Li <libo0929@gmail.com>
2021-03-04 20:28:34 +0800

    Updated scripts.

--- script/runFileMerge.py
+++ script/runFileMerge.py
@@ -1,13 +1,14 @@
 import os
 
-for energy in range(50, 90, 10):
-    sourceFiles = '/home/libo/workplace/CaloSim/script/output/gamma_' + str(energy) + 'GeV/CaloSim_gamma_' + str(energy) + 'GeV_*.slcio'
-    targetFile  = '/home/libo/workplace/CaloSim/script/output/gamma_' + str(energy) + 'GeV/CaloSim_gamma_' + str(energy) + 'GeV'
+particle='gamma'
+
+for energy in range(10, 90, 10):
+    sourceFiles = 'output/' + particle + '_' + str(energy) + 'GeV/CaloSim_' + particle + '_' + str(energy) + 'GeV_*.slcio'
+    targetFile  = 'output/' + particle + '_' + str(energy) + 'GeV/CaloSim_' + particle + '_' + str(energy) + 'GeV'
 
     os.system('lcio_merge_files ' + targetFile  + ' ' + sourceFiles)
 
-    #recofile = 'reco_gamma_' + str(energy) + 'GeV.root'
     os.system('./writeCaloHit ' + targetFile + '.slcio > /dev/null')
-    os.system('mv -f caloHits.root /home/libo/workplace/CaloSim/script/output/gamma_' + str(energy) + 'GeV')
+    os.system('mv -f caloHits.root output/' + particle + '_' + str(energy) + 'GeV')
 
     print(str(energy) + ' GeV done.')
