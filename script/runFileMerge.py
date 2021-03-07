64424aceea70827a63c49a392581a67f84d1f4a8
libo929 <libo929@163.com>
2021-03-07 17:28:37 +0800

    Added the doMerge option

--- script/runFileMerge.py
+++ script/runFileMerge.py
@@ -1,12 +1,15 @@
 import os
 
-particle='gamma'
+particle = 'gamma'
+
+doMerge = True
 
 for energy in range(10, 90, 10):
     sourceFiles = 'output/' + particle + '_' + str(energy) + 'GeV/CaloSim_' + particle + '_' + str(energy) + 'GeV_*.slcio'
     targetFile  = 'output/' + particle + '_' + str(energy) + 'GeV/CaloSim_' + particle + '_' + str(energy) + 'GeV'
 
-    os.system('lcio_merge_files ' + targetFile  + ' ' + sourceFiles)
+    if doMerge:
+        os.system('lcio_merge_files ' + targetFile  + ' ' + sourceFiles)
 
     os.system('./writeCaloHit ' + targetFile + '.slcio > /dev/null')
     os.system('mv -f caloHits.root output/' + particle + '_' + str(energy) + 'GeV')
